685110b5c63f96ff5afbf815aee9dc93550df4f0
Gil Estes <gil.estes@gmail.com>
2023-01-09 18:53:35 -0500

    first commit

---
+++ RandomNumberGenerator/RandomNumberGenerator/RandomNumberGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomNumberGeneratorApp.swift
+//  RandomNumberGenerator
+//
+//  Created by Gil Estes on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomNumberGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
